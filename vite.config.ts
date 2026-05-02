26b2f05df448d9ad7d6b496790849fe2d99fc13b
Adeena6 <187198944+Adeena6@users.noreply.github.com>
2026-05-02 03:07:06 -0700

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/-fashion-ecommerce-react-Website/' : '/',
+  base: mode === 'production'
+    ? '/fashion-ecommerce-website-/'
+    : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
