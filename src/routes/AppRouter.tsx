4b9d4a4243b8f657cf466cbdd0c6a9f1a7c37d19
Adeena6 <187198944+Adeena6@users.noreply.github.com>
2026-05-02 03:16:41 -0700

    run

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
@@ -24,40 +24,36 @@ import WinterCollection from "../pages/WinterCollection";
 import Brands from "../pages/Brands";
 import Blog from "../pages/Blog";
 
-const basename = import.meta.env.PROD ? "/-fashion-ecommerce-react-Website/" : "/";
-
 export default function AppRouter() {
   return (
-    <BrowserRouter basename={basename}>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/new-arrivals" element={<NewArrivals />} />
-          <Route path="/luxury-collection" element={<LuxuryCollection />} />
-          <Route path="/streetwear" element={<Streetwear />} />
-          <Route path="/summer-collection" element={<SummerCollection />} />
-          <Route path="/winter-collection" element={<WinterCollection />} />
-          <Route path="/brands" element={<Brands />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/new-arrivals" element={<NewArrivals />} />
+        <Route path="/luxury-collection" element={<LuxuryCollection />} />
+        <Route path="/streetwear" element={<Streetwear />} />
+        <Route path="/summer-collection" element={<SummerCollection />} />
+        <Route path="/winter-collection" element={<WinterCollection />} />
+        <Route path="/brands" element={<Brands />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
 
-          {/* Protected Routes */}
-          <Route path="/wishlist" element={<ProtectedRoute><Wishlist /></ProtectedRoute>} />
-          <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
-          <Route path="/order-success" element={<ProtectedRoute><OrderSuccess /></ProtectedRoute>} />
-          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        {/* Protected Routes */}
+        <Route path="/wishlist" element={<ProtectedRoute><Wishlist /></ProtectedRoute>} />
+        <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
+        <Route path="/order-success" element={<ProtectedRoute><OrderSuccess /></ProtectedRoute>} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
 
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
